2a2701627834aa8211a23e26e575654f6becf8d1
OldStunna27 <168038090+OldStunna27@users.noreply.github.com>
2026-06-23 10:17:02 -0500

    Removed Pricing Feartures

--- app/components/AppNavbar.vue
+++ app/components/AppNavbar.vue
@@ -4,7 +4,7 @@ const links = [
   { label: 'Services', to: '/services' },
   { label: 'About', to: '/about' },
   { label: 'Blog', to: '/blog' },
-  { label: 'Pricing', to: '/pricing' },
+  // { label: 'Pricing', to: '/pricing' },
   { label: 'Contact', to: '/contact' },
 ]
 
